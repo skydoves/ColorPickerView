bc5c0f1166d81e3f44d2195b00a95b15ba455d45
skydoves <skydoves@naver.com>
2019-08-07 15:57:15 +0900

    update travis & gradle build dependency configurations

--- spotless.license.java
+++ spotless.license.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 skydoves
+ * Designed and developed by 2017 skydoves (Jaewoong Eum)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
